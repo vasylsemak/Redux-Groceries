5baa1cb20c174b689a22b630ed08016a54d01e80
Vasyl Semak <vasylsmk1401@gmail.com>
2021-01-12 20:35:03 -0500

    modify AddGrocery component: add local state, form, button

--- client/components/AddGrocery.js
+++ client/components/AddGrocery.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-const AddGrocery = () => (
-  <div>AddGrocery</div>
-);
+class AddGrocery extends Component {
+  constructor() {
+    super();
+    this.state = { input: "" };
+
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleEnter(evt) {
+    if(evt.key === "Enter") {
+      console.log("KKK+> ", this.state.input)
+      this.props.add(this.state.input);
+      this.setState({ input: "" });
+    }
+  }
+
+  handleClick(evt) {
+    evt.preventDefault;
+    this.props.add(this.state.input);
+    this.setState({ input: "" });
+  }
+
+  render() {
+    return (
+      <div className="add-grocery">
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={evt => this.setState({ input: evt.target.value })}
+          onKeyDown={this.handleEnter}
+        />
+        <button onClick={this.handleClick}>Add Grocery</button>
+      </div>
+    )
+  }
+};
 
 export default AddGrocery;
